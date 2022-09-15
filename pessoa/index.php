21ae2061c8f9d563133e6fb792ae1825358eedf7
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 13:53:12 -0300

    adicionando modal de cadastro de pessoa

--- pessoa/index.php
+++ pessoa/index.php
@@ -2,11 +2,11 @@
 <?php include_once('../includes/navbar.php') ?>
 
 <?php
-    $sql = ("SELECT * FROM `pessoa` WHERE 1 = 1");
+$sql = ("SELECT * FROM `pessoa` WHERE 1 = 1");
 
-    $pessoa = $connection->connection()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+$pessoa = $connection->connection()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-    ?>
+?>
 
 <div class="container" id="container-principal">
     <div class="row">
@@ -16,10 +16,104 @@
         <div class="col-md-6 mt-4 text-end">
             <button class="btn btn-success" id="btn-cadastrar" type="button"><i class="fa-sharp fa-solid fa-user-plus"></i> CADASTRAR</button>
         </div>
+        <div class="modal" id="modal-cadastrar-pessoa" tabindex="-1">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header bg-success">
+                        <h3 class="modal-title text-light">Cadastrar</h3>
+                        <button type="button" class="btn-close bg-light me-2" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="form-cadastrar-pessoa" method="post">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <span class="error error-execute"></span>
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="form-label">Nome: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="nome" maxlength="100" placeholder="Informe o nome" autofocus>
+                                    <span class="error error-nome"></span>
+                                </div>
+                                <div class="col-md-3">
+                                    <label class="form-label">tipo: <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="tipo" id="">
+                                        <option value="1">Pessoa Fisíca</option>
+                                        <option value="2">Pessoa Juridica</option>
+                                    </select>
+                                    <span class="error error-tipo"></span>
+                                </div>
+                                <div class="col-md-3">
+                                    <label class="form-label">Sexo: <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="sexo" id="">
+                                        <option value="M">Masculino</option>
+                                        <option value="F">Feminino</option>
+                                    </select>
+                                    <span class="error error-sexo"></span>
+                                </div>
+                                <div class="col-md-4 mt-3">
+                                    <label class="form-label">CPF/CNPJ: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="documento" maxlength="20" placeholder="Informe o documento">
+                                    <span class="error error-documento"></span>
+                                </div>
+                                <div class="col-md-4 mt-3">
+                                    <label class="form-label">Cidade: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="cidade" maxlength="20" placeholder="Informe a cidade">
+                                    <span class="error error-cidade"></span>
+                                </div>
+                                <div class="col-md-4 mt-3">
+                                    <label class="form-label">CEP: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="cep" maxlength="20" placeholder="Informe o CEP">
+                                    <span class="error error-cep"></span>
+                                </div>
+                                <div class="col-md-5 mt-3">
+                                    <label class="form-label">Endereço: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="endereco" maxlength="20" placeholder="Informe o endereço">
+                                    <span class="error error-endereco"></span>
+                                </div>
+                                <div class="col-md-3 mt-3">
+                                    <label class="form-label">Número: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="numero" maxlength="20" placeholder="Informe o número">
+                                    <span class="error error-numero"></span>
+                                </div>
+                                <div class="col-md-4 mt-3">
+                                    <label class="form-label">Bairro: <span class="text-danger">*</span></label>
+                                    <input class="form-control" type="text" name="bairro" maxlength="20" placeholder="Informe o bairro">
+                                    <span class="error error-bairro"></span>
+                                </div>
+                                <div class="col-md-4 mt-3">
+                                    <label class="form-label">UF: <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="uf" id="">
+                                        <option value="AC">Acre</option>
+                                        <option value="AL">Alagoas</option>
+                                        <option value="AP">Amapá</option>
+                                        <option value="AM">Amazonas</option>
+                                        <option value="BA">Bahia</option>
+                                        <option value="CE">Ceará</option>
+                                        <option value="DF">Distrito Federal</option>
+                                        <option value="ES">Espírito Santo</option>
+                                    </select>
+                                    <span class="error error-uf"></span>
+                                </div>
+                                <div class="col-md-8 mt-3">
+                                    <label class="form-label">Complemento:</label>
+                                    <input class="form-control" type="text" name="complemento" maxlength="20" placeholder="Informe o complemento">
+                                    <span class="error error-complemento"></span>
+                                </div>
+
+                            </div>
+                            <hr>
+                            <div class="mt-4 text-end">
+                                <button class="btn btn-outline-danger" type="button" data-bs-dismiss="modal">CANCELAR</button>
+                                <button class="btn btn-success" id="btn-submit-cadastrar" type="submit"><i class="fa-sharp fa-solid fa-user-plus"></i> CADASTRAR</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-md-12">
             <hr>
         </div>
-
     </div>
 </div>
 
