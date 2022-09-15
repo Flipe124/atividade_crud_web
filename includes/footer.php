c1ee155c8d7fddd7d41b5c7fea52500f69dd8300
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 12:25:06 -0300

    adicionando importação de script

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <script src="../framework/Bootstrap/script.js"></script>
 <script src="../framework/JQuery/script.js"></script>
+<script src="../js/pessoa/script.js"></script>
 <script src="https://kit.fontawesome.com/52047f2aa2.js" crossorigin="anonymous"></script>
 
 </body>
