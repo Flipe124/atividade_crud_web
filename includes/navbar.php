06b109de548cc19b450136fceb3028594aa18d7c
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 15:04:54 -0300

    Estilizando navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,13 @@
-<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="fa-sharp fa-solid fa-code text-black"></i></a>
+    <a class="navbar-brand text-light" href="#"><i class="fa-sharp fa-solid fa-bars-progress"></i>FCAD</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link text-light" href="./">Início</a>
+        <a class="nav-link text-light" href="#">Quem somos</a>
       </div>
     </div>
   </div>
