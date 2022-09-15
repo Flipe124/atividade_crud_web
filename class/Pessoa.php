0f73ee831438d63e044604b9efd88e6257f9e359
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 12:25:36 -0300

    adicionando função create

--- class/Pessoa.php
+++ class/Pessoa.php
@@ -1,7 +1,9 @@
 <?php
 require("../../config/connection.php");
+require("../../class/Validator.php");
 
-class Pessoa{
+class Pessoa
+{
     protected $lastError = [];
     protected $lastMsg = null;
 
@@ -13,5 +15,31 @@ class Pessoa{
     public function lastMsg()
     {
         return $this->lastMsg;
+    }
+
+
+    function create($data)
+    {
+        try {
+            $this->lastError = [];
+
+            $nome = isset($data['name']) && $data['name'] != '' ? Validador::clearData($data['name']) : null;
+
+            if (!$nome) {
+                $this->lastError[] = array('msg' => MsgException::INFORME_NOME, 'field' => "name");
+            }
+
+            $connection = new Database();
+
+            $stmt = $connection->connection()->prepare(
+                'INSERT INTO `user` (`nome`) VALUES (:nome)'
+            );
+
+            $stmt->bindParam(":name",  $nome,  PDO::PARAM_STR);
+
+        } catch (\Throwable $th) {
+            $this->lastError[] = array('msg' => $th->getMessage(), 'field' => "execute");
+            return false;
+        }
     }
 }
