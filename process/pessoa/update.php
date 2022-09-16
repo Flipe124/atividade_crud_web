0e1a1725761c056e00213757f4b82aad3d316e5b
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 22:55:32 -0300

    adicionando obj update

---
+++ process/pessoa/update.php
@@ -0,0 +1,18 @@
+<?php
+
+require("../../class/Pessoa.php");
+
+$obj = new Pessoa();
+$obj->update($_POST);
+
+$response = array("error" => [], "msg" => null);
+
+if (count($obj->lastError()) > 0) {
+    $response["error"] = $obj->lastError();
+    $response["msg"] = null;
+} else {
+    $response["error"] = [];
+    $response["msg"] = $obj->lastMsg();
+}
+
+echo json_encode($response);
