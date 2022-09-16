02036f670ac51a4e1599d4dea337cbacead17f44
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-16 17:53:29 -0300

    adicionando novas exceções

--- class/MsgException.php
+++ class/MsgException.php
@@ -2,8 +2,30 @@
 class MsgException extends Exception{
 
     // INFORME O CAMPO
+
     const INFORME_NOME = "Informe o nome!";
 
-    
+    const INFORME_TIPO = "Informe o tipo!";
+
+    const INFORME_SEXO = "Informe o sexo!";
+
+    const INFORME_DOCUMENTO = "Informe o documento!";
+
+    const INFORME_CIDADE = "Informe a cidade!";
+
+    const INFORME_CEP = "Informe o CEP!";
+
+    const INFORME_ENDERECO = "Informe o endereço!";
+
+    const INFORME_NUMERO = "Informe o número!";
+
+    const INFORME_BAIRRO = "Informe o bairro!";
+
+    const INFORME_COMPLEMENTO = "Informe o complemento!";
+
+    const INFORME_UF = "Informe a UF!";
+
+    // 
+
     const USUARIO_NAO_EXISTE = "Usuário não encontrado!";
 }
