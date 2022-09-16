5926a1c75b26422a996b80bb2c6a614ae20fdc79
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-15 22:54:40 -0300

    adicionando link no navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#"><i class="fa-sharp fa-solid fa-bars-progress"></i>FCAD</a>
+    <a class="navbar-brand text-light ms-5" href="./"><i class="fa-sharp fa-solid fa-bars-progress"></i>FCAD</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
